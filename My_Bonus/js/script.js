15650a44284c66543c1145d0b7368f4fb5401e61
Vegadrone <spenaantonino@gmail.com>
2022-07-04 17:49:34 +0200

    Update: try to generate a card from inputs. Failed attempt

--- My_Bonus/js/script.js
+++ My_Bonus/js/script.js
@@ -3,10 +3,29 @@ const app = new Vue (
         el: '#root',
 
         data:{
+            imgSrc:"",
+            title:"",
+            imgSrcUpdate:"",
+            description:""
 
         },
 
         methods:{
+            generateCard: function(){
+                const wrapper = document.getElementById('wrapper');
+                wrapper.innerHTML += `<div class="row mt-5">
+                <div class="col-4">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="imgSrc" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">${{title}}</h5>
+                            <p class="card-text">${{description}}</p>
+                        </div>
+                    </div>
+                </div>
+              </div>`
+            this.imgSrc = this.imgSrcUpdate;
+            }
 
         },
     }
