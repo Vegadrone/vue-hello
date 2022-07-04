4ca508b64f81d9a67b8dd63ea6a6ceaa3821b267
Vegadrone <spenaantonino@gmail.com>
2022-07-04 17:51:23 +0200

    Update: working code

--- My_Bonus/js/script.js
+++ My_Bonus/js/script.js
@@ -12,18 +12,18 @@ const app = new Vue (
 
         methods:{
             generateCard: function(){
-                const wrapper = document.getElementById('wrapper');
-                wrapper.innerHTML += `<div class="row mt-5">
-                <div class="col-4">
-                    <div class="card" style="width: 18rem;">
-                        <img :src="imgSrc" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title fw-bold">${{title}}</h5>
-                            <p class="card-text">${{description}}</p>
-                        </div>
-                    </div>
-                </div>
-              </div>`
+            //     const wrapper = document.getElementById('wrapper');
+            //     wrapper.innerHTML += `<div class="row mt-5">
+            //     <div class="col-4">
+            //         <div class="card" style="width: 18rem;">
+            //             <img :src="imgSrc" class="card-img-top" alt="">
+            //             <div class="card-body">
+            //                 <h5 class="card-title fw-bold">{{title}}</h5>
+            //                 <p class="card-text">{{description}}</p>
+            //             </div>
+            //         </div>
+            //     </div>
+            //   </div>`
             this.imgSrc = this.imgSrcUpdate;
             }
 
