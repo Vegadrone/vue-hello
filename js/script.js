9d1f42589b4793f5bb8d5d039ee6eac6e8ecd0b3
Vegadrone <spenaantonino@gmail.com>
2022-07-04 16:24:36 +0200

    Update: change color on click

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const app = new Vue (
     {
         el: '#root',
+
         data: {
             message:'Ciao Luigi/Stefano/Riccardo, so che mi stai guardando.',
-            textInput:""
+            textInput:"",
+            colorClasses: 'txt-lime'
+        },
+
+        methods:{
+            changeColorOnClick: function(){
+                if(this.colorClasses == 'txt-lime'){
+                    this.colorClasses = 'txt-salmon';
+                } else {
+                    this.colorClasses = 'txt-lime';
+                }
+            }
         }
     }
 )
